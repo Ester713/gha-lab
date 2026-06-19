896d4cc20d5f5d90c2a907490bae20536db90ca9
Ester713 <kleenitfacmgt@gmail.com>
2026-06-19 14:28:07 -0500

    fix sum test js file

--- app/sum.test.js
+++ app/sum.test.js
@@ -3,7 +3,7 @@ const assert = require('node:assert');
 const sum = require('./sum');
 
 test('sum adds two numbers', () => {
-  assert.strictEqual(sum(2, 3), 6);;
+  assert.strictEqual(sum(2, 3), 5);
 });
 
 test('sum works with negative numbers', () => {
